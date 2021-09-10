f9fc8b7cb0a959d9558f059f3a78be4852b351e4
0Mayank <1mayanktomar@gmail.com>
2021-09-10 17:40:40 +0530

    implemented guild sharding

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,43 @@
+use std::env;
+
 use serenity::{
     async_trait,
     model::{channel::Message, gateway::Ready},
     prelude::*,
 };
 
+// Serenity implements transparent sharding in a way that you do not need to
+// manually handle separate processes or connections manually.
+//
+// Transparent sharding is useful for a shared cache. Instead of having caches
+// with duplicated data, a shared cache means all your data can be easily
+// accessible across all shards.
+//
+// If your bot is on many guilds - or over the maximum of 2500 - then you
+// should/must use guild sharding.
+//
+// This is an example file showing how guild sharding works. For this to
+// properly be able to be seen in effect, your bot should be in at least 2
+// guilds.
+//
+// Taking a scenario of 2 guilds, try saying "!ping" in one guild. It should
+// print either "0" or "1" in the console. Saying "!ping" in the other guild,
+// it should cache the other number in the console. This confirms that guild
+// sharding works.
 struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
-    // Set a handler for the `message` event - so that whenever a new message
-    // is received - the closure (or function) passed will be called.
-    //
-    // Event handlers are dispatched through a threadpool, and so multiple
-    // events can be dispatched simultaneously.
     async fn message(&self, ctx: Context, msg: Message) {
         if msg.content == "!ping" {
-            // Sending a message can fail, due to a network error, an
-            // authentication error, or lack of permissions to post in the
-            // channel, so log to stdout when some error happens, with a
-            // description of it.
+            println!("Shard {}", ctx.shard_id);
+
             if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
                 println!("Error sending message: {:?}", why);
             }
         }
     }
 
-    // Set a handler to be called on the `ready` event. This is called when a
-    // shard is booted, and a READY payload is sent by Discord. This payload
-    // contains data like the current user's guild Ids, current user data,
-    // private channels, and more.
-    //
-    // In this case, just print what the current user's username is.
     async fn ready(&self, _: Context, ready: Ready) {
         println!("{} is connected!", ready.user.name);
     }
@@ -39,19 +46,17 @@ impl EventHandler for Handler {
 #[tokio::main]
 async fn main() {
     // Configure the client with your Discord bot token in the environment.
-    let token = "ODg1ODUzMDEzMTIwNTM2NTc3.YTtE6A.ZWnWpU4ChjgmgCXlsqy935zAV34";
-
-    // Create a new instance of the Client, logging in as a bot. This will
-    // automatically prepend your bot token with "Bot ", which is a requirement
-    // by Discord for bot users.
+    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
     let mut client =
         Client::builder(&token).event_handler(Handler).await.expect("Err creating client");
 
-    // Finally, start a single shard, and start listening to events.
+    // The total number of shards to use. The "current shard number" of a
+    // shard - that is, the shard it is assigned to - is indexed at 0,
+    // while the total shard count is indexed at 1.
     //
-    // Shards will automatically attempt to reconnect, and will perform
-    // exponential backoff until it reconnects.
-    if let Err(why) = client.start().await {
+    // This means if you have 5 shards, your total shard count will be 5, while
+    // each shard will be assigned numbers 0 through 4.
+    if let Err(why) = client.start_shards(2).await {
         println!("Client error: {:?}", why);
     }
 }
